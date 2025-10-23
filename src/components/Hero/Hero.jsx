76e7ac0a42c32fc8ca7780e17b1a093ecda0f9e1
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-10-23 22:58:16 +0530

    hero-section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import Grocery from "../../assets/grocery.png"
+import Button from '../Button/Button'
 
 const Hero = () => {
   return (
     <section>
-        <div className='max-w-[1400px] mx-auto px-10'>
+        <div className='min-h-screen max-w-[1400px] mx-auto px-10 flex items-center pt-28'>
             {/* Hero content */}
-            <div>
+            <div className='flex-1'>
                 <span className='bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full'>Export Best Quality..</span>
-                <h1 className='text-7xl font-bold '>
-                    Tasty Organic <span className='text-orange-500'>Fruits</span> & <span className='text-orange-500'>Veggies</span> In Your City 
+                <h1 className='text-7xl/20 font-bold mt-4'>
+                    Tasty Organic <span className='text-orange-500'>Fruits</span> & <span className='text-orange-500'>Veggies</span> <br />In Your City 
                 </h1>
-                <p className='text-zinc-600 text-lg '>
+                <p className='text-zinc-600 text-lg max-w-[530px] mt-5 mb-8'>
                     Bred for a high content of beneficial substances. Our products are all fresh and healthy.
                 </p>
+                <Button btnText="Shop Now" />
             </div>
 
             {/* hero image */}
-            <div>
+            <div className='flex-1'>
                 <img src={Grocery} alt="" />
             </div>
         </div>
