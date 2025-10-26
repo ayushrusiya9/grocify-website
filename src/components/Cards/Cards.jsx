e5d5ea625355dc61eb9b4e3c3452e99af10a580a
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-10-26 11:59:47 +0530

    update

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FaHeart, FaPlus } from 'react-icons/fa'
+import Button from '../Button/Button'
+
+const Cards = ({image,name,price}) => {
+    return (
+        <div className='bg-zinc-100 p-5 rounded-xl'>
+            {/* card icons */}
+            <div className='flex justify-between'>
+                <span className='text-3xl text-zinc-300 '>
+                    <FaHeart/>
+                </span>
+                <button className='bg-linear-to-b from-orange-400 to-orange-500 text-white px-3 py-2 rounded-lg md:text-lg text-md  hover:scale-105 hover:to-orange-600 transition-all duration-300 cursor-pointer'>
+                    <FaPlus/>
+                </button>
+            </div>
+
+            {/* Card image */}
+            <div className='w-full h-50'>
+                <img src={image} className='w-full h-full object-contain mx-auto' />
+            </div>
+
+            {/* card content */}
+            <div className='text-center'>
+                <h3 className='text-2xl'>{name}</h3>
+                <p className='text-2xl font-bold mt-4 mb-3'>&#8377; {price.toFixed(2)}</p>
+                <Button btnText='Shop Now'/>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,14 +1,29 @@
 import React, { useState } from 'react'
 import Heading from '../Heading/Heading'
+import ProductsList from '../ProductList/ProductsList'
+import Cards from '../Cards/Cards'
+import Button from '../Button/Button'
+
 
 const Products = () => {
 
     const categories =['All', 'Fruits', 'Vegetables', 'Dairy', 'SeaFood']
     const [activeTab, setactiveTab] = useState('All')
 
+    let filteredItems = activeTab === 'All' ? ProductsList : ProductsList.filter(item => item.category === activeTab)
+
+
+    const renderCards = filteredItems.slice(0,8).map(product=>{
+        return (
+            <Cards name={product.name} image={product.image} price={product.price}/>
+        )
+    })
+
+
     return (
         <section>
             <div className='max-w-[1400px] mx-auto px-10 py-20'>
+                {/* heading */}
                 <Heading highlight='Our' heading='Products' />
 
                 {/* Tabs */}
@@ -24,9 +39,22 @@ const Products = () => {
                         )
                     }))}
                 </div>
+
+                {/* product listing */}
+                <div className='grid grid-cols-4 mt-20 gap-9'>
+                    {renderCards}
+                </div>
+
+                {/* view all button */}
+                <div className='mt-15 mx-auto w-fit'>
+                    <Button btnText='View All'/>
+                </div>
             </div>
+
         </section>
     )
 }
 
 export default Products
+
+
